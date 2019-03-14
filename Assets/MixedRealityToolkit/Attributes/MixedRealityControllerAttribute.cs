ee682efee0a3e45c7bb1ff7744eeae22e5a26b9e
Will Wei <wiwei@microsoft.com>
2019-03-14 11:18:37 -0700

    Fixes MixedRealityControllerAttribute to work on the .NET scripting backend
    Types.GetCustomAttributes() doesn't exist on .NET UWP, so we have to use some indirection to get attributes instead.

--- Assets/MixedRealityToolkit/Attributes/MixedRealityControllerAttribute.cs
+++ Assets/MixedRealityToolkit/Attributes/MixedRealityControllerAttribute.cs
@@ -5,6 +5,7 @@ using Microsoft.MixedReality.Toolkit.Core.Definitions.Devices;
 using Microsoft.MixedReality.Toolkit.Core.Definitions.Utilities;
 using System;
 using System.Linq;
+using System.Reflection;
 
 namespace Microsoft.MixedReality.Toolkit.Core.Attributes
 {
@@ -56,9 +57,18 @@ namespace Microsoft.MixedReality.Toolkit.Core.Attributes
         /// <summary>
         /// Convenience function for retrieving the attribute given a certain class type.
         /// </summary>
+        /// <remarks>
+        /// This function is only available in a UnityEditor context.
+        /// </remarks>
         public static MixedRealityControllerAttribute Find(Type type)
         {
+#if WINDOWS_UWP && !ENABLE_IL2CPP
+            // Type.GetCustomAttributes() doesn't exist on UWP .NET, so we have to indirectly use
+            // the TypeInfo instead.
+            return type.GetTypeInfo().GetCustomAttributes(typeof(MixedRealityControllerAttribute), true).FirstOrDefault() as MixedRealityControllerAttribute;
+#else
             return type.GetCustomAttributes(typeof(MixedRealityControllerAttribute), true).FirstOrDefault() as MixedRealityControllerAttribute;
+#endif
         }
     }
 }
